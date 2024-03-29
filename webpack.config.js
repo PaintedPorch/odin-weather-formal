cfd423ecdccb19661d0d6ab695ef2e94e1feeb84
Lars Bakker <Lars.bakker@casema.nl>
2024-03-29 16:57:57 +0100

    Add revisions

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {                                
+    static: {                                 
+      directory: path.join(__dirname, "dist"),
+    },                                        
+  },
+  devtool: 'eval-source-map',
+  plugins: [
+    new ESLintPlugin(),
+    new CleanWebpackPlugin({
+      verbose: true
+    }),
+    new HtmlWebpackPlugin({
+      title: "Odin",
+      template: './src/index.html', // If using only one html file
+      inject: 'body'
+    }),
+  ],
+  module: {
+    rules: [
+      { // Use Css
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+      },
+      { // Use images
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[name][ext][query]',
+        }
+      }, 
+      { // Use fonts
+        test      : /\.(woff2?|ttf|eot)(\?v=\w+)?$/,
+        type      : 'asset/resource',
+        generator : {
+          filename : 'assets/fonts/[name][ext][query]',
+        }
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+      }
+    ]
+  }
+};
